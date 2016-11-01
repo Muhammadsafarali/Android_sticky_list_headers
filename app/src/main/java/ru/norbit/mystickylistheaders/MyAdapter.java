71479240402b9a86ffe0ea7ce2f73eaed97e5dd9
Алисултанов Сафарали Энверович <safarali.alisultanov@norbit.ru>
2016-11-01 16:04:23 +0300

    Вернул getView   - Помещаю SwipeLayout на строку listView;

--- app/src/main/java/ru/norbit/mystickylistheaders/MyAdapter.java
+++ app/src/main/java/ru/norbit/mystickylistheaders/MyAdapter.java
@@ -1,15 +1,20 @@
 package ru.norbit.mystickylistheaders;
 
 import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.daimajia.swipe.adapters.BaseSwipeAdapter;
 import com.daimajia.swipe.SimpleSwipeListener;
 import com.daimajia.swipe.SwipeLayout;
+import com.daimajia.swipe.implments.SwipeItemAdapterMangerImpl;
+import com.daimajia.swipe.implments.SwipeItemMangerImpl;
+import com.daimajia.swipe.implments.SwipeItemRecyclerMangerImpl;
 
 import java.util.List;
 
@@ -19,7 +24,7 @@ import se.emilsjolander.stickylistheaders.StickyListHeadersAdapter;
  * Created by safarali.alisultanov on 26.10.2016.
  */
 
-public class MyAdapter extends BaseSwipeAdapter implements StickyListHeadersAdapter {
+public class MyAdapter extends BaseAdapter implements StickyListHeadersAdapter, SwipeLayout.SwipeListener {
 
     private List<String> countries;
     private LayoutInflater inflater;
@@ -46,43 +51,75 @@ public class MyAdapter extends BaseSwipeAdapter implements StickyListHeadersAdap
         return i;
     }
 
-    @Override
-    public int getSwipeLayoutResourceId(int position) {
-        return R.id.swipe;
+    private void printMsg(String msg) {
+        Log.e("LOG", msg);
     }
 
+
+
     @Override
-    public View generateView(final int position, ViewGroup parent) {
-        View v = inflater.inflate(R.layout.test_list_item_layout, null);
-        SwipeLayout swipeLayout = (SwipeLayout)v.findViewById(getSwipeLayoutResourceId(position));
-        swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
-        swipeLayout.addSwipeListener(new SimpleSwipeListener() {
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            printMsg("null convertView -> " + position);
+            holder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.test_list_item_layout, parent, false);
+            SwipeLayout swipeLayout = (SwipeLayout)convertView.findViewById(R.id.swipe);
+            swipeLayout.addSwipeListener(new SimpleSwipeListener() {
+                @Override
+                public void onOpen(SwipeLayout layout) {
+                    printMsg("on swipe open -> " + position);
+                }
+            });
+            holder.text = (TextView)convertView.findViewById(R.id.text);
+            convertView.setTag(holder);
+        }
+        else {
+            printMsg("convertView -> " + position);
+            holder = (ViewHolder)convertView.getTag();
+        }
 
-            @Override
-            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
-                // if (xvel <= 0) don't remove
-                // if (xvel > 0) remove
-                Log.e("LOG", "onHandRelease xvel = " + xvel + " yvel = " + yvel);
-            }
-
-        });
-        swipeLayout.setOnDoubleClickListener(new SwipeLayout.DoubleClickListener() {
-            @Override
-            public void onDoubleClick(SwipeLayout layout, boolean surface) {
-                Log.e("LOG", "double click");
-            }
-        });
-        ViewHolder holder = new ViewHolder();;
-        holder.text = (TextView) swipeLayout.findViewById(R.id.text);
-        swipeLayout.setTag(holder);
         holder.text.setText(countries.get(position));
-
-        return v;
+        return convertView;
     }
 
-    @Override
-    public void fillValues(int position, View convertView) {
-    }
+//    @Override
+//    public int getSwipeLayoutResourceId(int position) {
+//        return R.id.swipe;
+//    }
+//
+//    @Override
+//    public View generateView(final int position, ViewGroup parent) {
+//        View v = inflater.inflate(R.layout.test_list_item_layout, null);
+//        SwipeLayout swipeLayout = (SwipeLayout)v.findViewById(getSwipeLayoutResourceId(position));
+//        swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
+//        swipeLayout.addSwipeListener(new SimpleSwipeListener() {
+//
+//            @Override
+//            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+//                // if (xvel <= 0) don't remove
+//                // if (xvel > 0) remove
+//                Log.e("LOG", "onHandRelease xvel = " + xvel + " yvel = " + yvel);
+//            }
+//
+//        });
+//        swipeLayout.setOnDoubleClickListener(new SwipeLayout.DoubleClickListener() {
+//            @Override
+//            public void onDoubleClick(SwipeLayout layout, boolean surface) {
+//                Log.e("LOG", "double click");
+//            }
+//        });
+//        ViewHolder holder = new ViewHolder();;
+//        holder.text = (TextView) swipeLayout.findViewById(R.id.text);
+//        swipeLayout.setTag(holder);
+//        holder.text.setText(countries.get(position));
+//
+//        return v;
+//    }
+//
+//    @Override
+//    public void fillValues(int position, View convertView) {
+//    }
 
 
     @Override
@@ -107,6 +144,36 @@ public class MyAdapter extends BaseSwipeAdapter implements StickyListHeadersAdap
 //        Log.e("LOG", "tmp");
         long ch = countries.get(position).subSequence(0,1).charAt(0);
         return ch;
+    }
+
+    @Override
+    public void onStartOpen(SwipeLayout layout) {
+
+    }
+
+    @Override
+    public void onOpen(SwipeLayout layout) {
+
+    }
+
+    @Override
+    public void onStartClose(SwipeLayout layout) {
+
+    }
+
+    @Override
+    public void onClose(SwipeLayout layout) {
+
+    }
+
+    @Override
+    public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+
+    }
+
+    @Override
+    public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+
     }
 
     class HeaderViewHolder {
